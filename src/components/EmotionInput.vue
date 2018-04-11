cca3e8a05b0dbe6dc98399c933a0560b7c733cc9
tzmanics <tzmanics@gmail.com>
2018-04-11 17:05:24 -0400

    add moar styling

--- src/components/EmotionInput.vue
+++ src/components/EmotionInput.vue
@@ -98,4 +98,15 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
+  .emotion-box {
+    background-color: rgba(255, 255, 255, 0.5);
+    margin: 0 auto;
+    min-width: 600px;
+    padding: 15px;
+    width: 47%;
+  }
+
+  .k-chart {
+    width: 90%;
+  }
 </style>
